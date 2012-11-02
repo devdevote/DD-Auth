c43d05b4a4f4a319789330fe91e53ef290cfe80f
Ramiro Rodriguez <ramirors@gmail.com>
2012-11-01 21:05:59 -0600

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -245,7 +245,7 @@ $config['encryption_key'] = 'SET YOUR ENCRYPTION KEY';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
+$config['sess_cookie_name']		= 'cisession';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
